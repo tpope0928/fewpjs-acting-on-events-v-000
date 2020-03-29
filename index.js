8fe68739dca30b2249757b4d82c7aad2b978b755
Thomas Pope <tpope0928@gmail.com>
2020-03-29 18:04:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ function moveDodgerLeft() {
     dodger.style.left = ${left - 1}px
   }
 }
+
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
