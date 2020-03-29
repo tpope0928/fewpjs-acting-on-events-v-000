78e6e2bace7f94c7ae99a94dbc9791b654a4b41c
Thomas Pope <tpope0928@gmail.com>
2020-03-29 18:03:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code here
+var dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
+  if (left > 0) {
+    dodger.style.left = ${left - 1}px
+  }
+}
+
